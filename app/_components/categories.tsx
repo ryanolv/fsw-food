9a781557102c07d15dfeb7e6ca21c2ed8f3d3ae4
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-03 20:50:31 -0300

    feat: ensure scrollbar is hidden on categories container

--- app/_components/categories.tsx
+++ app/_components/categories.tsx
@@ -4,7 +4,7 @@ import CategoryDetails from "./category";
 const Categories = async () => {
   const categories = await db.category.findMany({});
   return (
-    <div className="flex gap-2 overflow-x-auto whitespace-nowrap px-5">
+    <div className="flex gap-2 overflow-x-auto whitespace-nowrap px-5 no-scrollbar ">
       {categories.map((category) => (
         <CategoryDetails key={category.id} category={category} />
       ))}
