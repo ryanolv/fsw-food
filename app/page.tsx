048469bc15a8ad12b385a0fc15b41f0bb34a3523
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-03 16:00:03 -0300

    feat: add pizza banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Categories from "./_components/categories";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -8,6 +9,9 @@ export default function Home() {
       <Header />
       <Search />
       <Categories />
+      <div className="flex justify-center">
+        <Image src="/Banner-Pizza.png" alt="Banner" width={390} height={150} />
+      </div>
     </div>
   );
 }
