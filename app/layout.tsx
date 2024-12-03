b32f8052deff90543eb6314b28575c5147312633
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-03 15:47:35 -0300

    feat: add background color to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F4F4F5]`}
       >
         {children}
       </body>
