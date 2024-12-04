97c3d0e3e9628d3af155360792784143424512ab
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-04 13:05:08 -0300

    feat: ensure product page is shown when product item is clicked on home page

--- app/_components/product-details.tsx
+++ app/_components/product-details.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { productDTO } from "../_data/get-products";
 import { Badge } from "./ui/badge";
 import { calculateFinalPrice, formatPrice } from "../_helpers/prices";
+import Link from "next/link";
 
 interface ProductDetailsProps {
   product: productDTO;
@@ -9,33 +10,35 @@ interface ProductDetailsProps {
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
   return (
-    <div className="min-w-36">
-      <div className="relative min-h-[150px]">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          fill
-          className="rounded-xl object-cover"
-        />
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute top-2 right-2 ">
-            {product.discountPercentage}% OFF
-          </Badge>
-        )}
-      </div>
-      <div>
-        <h2 className="text-sm truncate">{product.name}</h2>
-        <div className="flex gap-1 items-center">
-          <h3 className="font-semibold">
-            {calculateFinalPrice(product.price, product.discountPercentage)}
-          </h3>
-          <h3 className="text-xs text-muted-foreground line-through">
-            {formatPrice(product.price)}
-          </h3>
+    <Link className="min-w-36" href={`../products/${product.id}`}>
+      <div className="w-full">
+        <div className="relative min-h-[150px]">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            fill
+            className="rounded-xl object-cover"
+          />
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute top-2 right-2 ">
+              {product.discountPercentage}% OFF
+            </Badge>
+          )}
         </div>
-        <p className="text-xs text-muted-foreground">{product.restaurant}</p>
+        <div>
+          <h2 className="text-sm truncate">{product.name}</h2>
+          <div className="flex gap-1 items-center">
+            <h3 className="font-semibold">
+              {calculateFinalPrice(product.price, product.discountPercentage)}
+            </h3>
+            <h3 className="text-xs text-muted-foreground line-through">
+              {formatPrice(product.price)}
+            </h3>
+          </div>
+          <p className="text-xs text-muted-foreground">{product.restaurant}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
