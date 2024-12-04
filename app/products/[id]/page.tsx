acf8be7e0d2cb8ad28c5b93183562f85b8c2aaaf
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-04 13:21:18 -0300

    feat: add details restaurant and product name

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarImage } from "@/app/_components/ui/avatar";
 import { getUniqueProduct } from "@/app/_data/get-products";
 import ImageProduct from "./_components/image-product";
 
@@ -14,6 +15,25 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
       {/* Imagem */}
       <ImageProduct product={product} />
       {/* Detalhes */}
+      <div>
+        {/* Detalhes do restaurante o nome do produto */}
+        <div>
+          <div className="flex gap-1 text-xs text-muted-foreground items-center">
+            <Avatar className="w-5 h-5">
+              <AvatarImage src={product.restaurant.imageUrl} />
+            </Avatar>
+            <span>{product.restaurant.name}</span>
+          </div>
+          <h1 className="font-semibold text-xl">{product.name}</h1>
+        </div>
+        {/* Valor e quantidade do produto */}
+
+        {/* Detalhes da entrega */}
+
+        {/* About  */}
+
+        {/* Produtos adicionais */}
+      </div>
     </div>
   );
 };
