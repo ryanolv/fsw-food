0286fe81470d2a42e009dc5a27a07eda53075124
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-05 14:22:09 -0300

    fix: ensure home page is returned when the logo is clicked

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/Logo.png" alt="Logo" width={100} height={30} />
+      <Link href="/">
+        <Image src="/Logo.png" alt="Logo" width={100} height={30} />
+      </Link>
       <Button variant="ghost">
         <MenuIcon />
       </Button>
