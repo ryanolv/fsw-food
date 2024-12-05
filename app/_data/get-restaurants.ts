8f1a9eb889c4af7859b52a465f2ef55e5f2fd669
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-05 13:11:25 -0300

    feat: add categories to RestaurantDTO and getEspecificRestaurant function

--- app/_data/get-restaurants.ts
+++ app/_data/get-restaurants.ts
@@ -1,5 +1,6 @@
 import { Decimal } from "@prisma/client/runtime/library";
 import { db } from "../_lib/prisma";
+import { Category } from "@prisma/client";
 
 export interface RestaurantDTO {
   id: string;
@@ -7,11 +8,42 @@ export interface RestaurantDTO {
   imageUrl: string;
   deliveryFee: Decimal;
   deliveryTimeMinutes: number;
+  categories: Category[];
 }
 
 export const getRestaurants = async (): Promise<RestaurantDTO[]> => {
   const restaurants = await db.restaurant.findMany({
     take: 5,
+    select: {
+      id: true,
+      name: true,
+      imageUrl: true,
+      deliveryFee: true,
+      deliveryTimeMinutes: true,
+      categories: true,
+    },
   });
   return restaurants;
+};
+
+export const getEspecificRestaurant = async (
+  id: string,
+): Promise<RestaurantDTO> => {
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      imageUrl: true,
+      deliveryFee: true,
+      deliveryTimeMinutes: true,
+      categories: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+  return restaurant;
 };
