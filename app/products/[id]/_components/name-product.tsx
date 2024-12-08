9a9313003eb1fef65f77316d25e981924353dac8
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-12-08 15:59:46 -0300

    feat: ensure restaurant pageis shown when user  will click at restaurant icon on product page

--- app/products/[id]/_components/name-product.tsx
+++ app/products/[id]/_components/name-product.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarImage } from "@/app/_components/ui/avatar";
 import { ProductDTO } from "@/app/_data/get-products";
+import Link from "next/link";
 
 interface NameProductProps {
   product: ProductDTO;
@@ -8,12 +9,14 @@ interface NameProductProps {
 const NameProduct = ({ product }: NameProductProps) => {
   return (
     <div>
-      <div className="flex gap-1 text-xs text-muted-foreground items-center">
-        <Avatar className="w-5 h-5">
-          <AvatarImage src={product.restaurant.imageUrl} />
-        </Avatar>
-        <span>{product.restaurant.name}</span>
-      </div>
+      <Link href={`/restaurants/${product.restaurant.id}`}>
+        <div className="flex gap-1 text-xs text-muted-foreground items-center">
+          <Avatar className="w-5 h-5">
+            <AvatarImage src={product.restaurant.imageUrl} />
+          </Avatar>
+          <span>{product.restaurant.name}</span>
+        </div>
+      </Link>
       <h1 className="font-semibold text-xl">{product.name}</h1>
     </div>
   );
